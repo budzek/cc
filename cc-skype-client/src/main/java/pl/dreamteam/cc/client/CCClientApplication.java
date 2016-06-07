657ce33f430ed5b11286dad02a6a9e5d3dda9382
budzek <budzek@tlen.pl>
2016-06-07 23:54:01 +0200

    fix for last commit, removing spring from client application

--- cc-skype-client/src/main/java/pl/dreamteam/cc/client/CCClientApplication.java
+++ cc-skype-client/src/main/java/pl/dreamteam/cc/client/CCClientApplication.java
@@ -1,27 +1,15 @@
 package pl.dreamteam.cc.client;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 import pl.dreamteam.cc.dto.Status;
 
 /**
  * Created by abu on 03.06.2016.
  */
-@SpringBootApplication
-
-@ImportResource("classpath:/spring/spring-config.xml")
 
 public class CCClientApplication {
-//    @Autowired
-//    ServiceFactory serviceFactory;
-
 
     public static void main(String[] args) {
         ServiceFactory.getInstance().getConsultantService().setStatus("LOL", Status.OFFLINE);
-
-
     }
 
 }
